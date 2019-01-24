f072d55dc67e82c35d158f62a544eda2d70f1876
avnovoselov <avnovoselov@gmail.com>
2019-01-24 20:58:08 +0300

    add : InterfaceDaemon add : AbstractDaemon add : Arguments

---
+++ src/Small/AbstractDaemon.php
@@ -0,0 +1,218 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anatoliy Novoselov
+ * Date: 22.01.2019
+ * Time: 19:43
+ */
+
+namespace Small;
+
+/**
+ * Class Daemon
+ * @package Small
+ */
+abstract class AbstractDaemon implements InterfaceDaemon
+{
+	/**
+	 * Имя демона
+	 *
+	 * @var string
+	 */
+	public $name;
+
+	/**
+	 * Дата запуска демона в формате ISO-8601
+	 *    <?php
+	 *        ...
+	 *        $this->startDate = date('c');
+	 * @var string
+	 */
+	protected $startDate;
+
+	/**
+	 * Дата последней итерации демона в формате ISO-8601
+	 *
+	 * @var string
+	 */
+	protected $lastRunDate;
+
+	/**
+	 * Задержка между итерациями демона
+	 * указывается в секундах
+	 *
+	 * @var int
+	 */
+	protected $sleep;
+
+	/**
+	 * Флаг проверять ли зависимости перед началом каждой итерации
+	 *
+	 * @var bool
+	 */
+	protected $skippable;
+
+	/**
+	 * @var Arguments
+	 *  Объект с аргументами запуска скрипта
+	 */
+	protected $arguments;
+
+	/**
+	 * Вывод приветственного сообщения при запуске демона
+	 *
+	 * @return void
+	 */
+	public function printWelcome()
+	{
+		$this->printName();
+		$this->printStartDate();
+	}
+
+	/**
+	 * Конструктор класса
+	 *    пример вызова:
+	 *
+	 *    file: Foo.php
+	 *    class Foo extends \Small\AbstractDaemon {
+	 *        public function process() {
+	 *            echo PHP_EOL . "bar==" . $this->arguments->get('bar');
+	 *            // your code here
+	 *        }
+	 *    }
+	 *
+	 *    file: daemon/foo.php
+	 *    $foo = new Foo('Foo', 3, true);
+	 *    $foo->arguments([
+	 *       'bar' => 1,
+	 *    ]);
+	 *
+	 *    shell/> php daemon/foo.php --bar=5
+	 *        bar==5
+	 *
+	 * Имя демона
+	 * @param string $name
+	 * @see AbstractDaemon::name
+	 *
+	 * Задержка между итерациями
+	 * @param int $sleep
+	 * @see AbstractDaemon::$sleep
+	 *
+	 * Флаг, могут ли быть пропущенные итерации (напр.: отсутствие соединение с БД)
+	 * @param bool $skippable
+	 * @see AbstractDaemon::$skippable
+	 *
+	 * @return InterfaceDaemon $this;
+	 */
+	public function __construct(string $name, int $sleep = 1, bool $skippable = true)
+	{
+		$this->name = $name;
+		$this->startDate = date('c');
+		$this->sleep = $sleep;
+		$this->skippable = $skippable;
+
+		return $this;
+	}
+
+	/**
+	 * Добавляет возможность получать аргументы, переданные при вызове скрипта
+	 *
+	 * аргументы по умолчанию
+	 * @param array $defaultArguments
+	 *
+	 *    Напр.:
+	 *    <?php
+	 *    ...
+	 *    $foo->arguments(["myparam" => 1]);
+	 *
+	 *    для скрипт, вызванного без параметров shell/> php foo.php
+	 *        echo $this->args->get('myparam'); // 1
+	 *
+	 *    для скрипта shell/> php foo.php --myparam=3
+	 *        echo $this->args->get('myparam'); // 3
+	 *
+	 * @return AbstractDaemon $this;
+	 */
+	final public function arguments(array $defaultArguments = [])
+	{
+		$this->arguments = new Arguments($defaultArguments);
+		$this->arguments->process();
+
+		return $this;
+	}
+
+	/**
+	 * Запуск демона
+	 *    реализует бесконечный цикл, в каждой итерации которого выполняет метод @see InterfaceDaemon::process
+	 *
+	 * @return void
+	 */
+	final public function run()
+	{
+		$this->printWelcome();
+
+		for (; ;) {
+			// проверяет удовлетворены ли все зависимости
+			// для выполнения итерации
+			if ($this->skip()) {
+				$this->printSkip();
+				sleep($this->sleep);
+				continue;
+			}
+
+			$lastRunDate = date('c');
+			// запускаем итерацию
+			static::process();
+
+			$this->setLastRunDate($lastRunDate);
+			// ждем установленное количество секунд для выполнения следующей итерации
+			sleep($this->sleep);
+		}
+	}
+
+	/**
+	 * Выводит сообщение вида
+	 *    > Daemon: <имя демона>
+	 *
+	 * @return void
+	 */
+	final protected function printName()
+	{
+		echo PHP_EOL . " Daemon: {$this->name}";
+	}
+
+	/**
+	 * Выводит сообщение вида
+	 *    > start at <дата и время запуска>
+	 *
+	 * @return void
+	 */
+	final protected function printStartDate()
+	{
+		echo PHP_EOL . " start at {$this->startDate}";
+	}
+
+	/**
+	 * Выводит сообщение вида
+	 *    > iteration skipped at <дата и время пропуска итерации>
+	 *
+	 * @return void
+	 */
+	protected function printSkip()
+	{
+		echo PHP_EOL . " iteration skipped at " . date('c');
+	}
+
+	/**
+	 * Устанавливает время последнего запуска
+	 *    если передан параметр $lastRunDate будет установлено его значение,
+	 *    в противном случае - текущие дата и время
+	 *
+	 * @param string $lastRunDate
+	 * @return void
+	 */
+	final protected function setLastRunDate(string $lastRunDate = '')
+	{
+		$this->lastRunDate = $lastRunDate ?: date('c');
+	}
+}
---
+++ src/Small/Arguments.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anatoliy Novoselov
+ * Date: 22.01.2019
+ * Time: 20:08
+ */
+
+namespace Small;
+
+/**
+ * Class Arguments
+ * @package Small
+ */
+class Arguments
+{
+	/**
+	 * Хранилище значений аргументов
+	 *
+	 * @var array
+	 */
+	protected $arguments = [];
+
+	/**
+	 * Разбирает аргументы, переданные в скрипт
+	 *
+	 * @return Arguments $this;
+	 */
+	final public function process()
+	{
+		// собираем параметр $longopts для функции getopt($shortopts, $longopts)
+		// приводим ключи массива значений аргументов по умолчанию к виду ["<key>":: => <defaultValue>]
+		// :: - указывает на необязательный аргумент
+		$_arguments = array_map(function ($el) {
+			return "{$el}::";
+		}, array_keys($this->arguments));
+
+		$option = getopt("", $_arguments);
+
+		// перезаписываем значения по умолчанию переданными аргументами
+		$this->arguments = array_merge($this->arguments, $option);
+
+		return $this;
+	}
+
+	/**
+	 * Возвращает значение аргумента
+	 *
+	 * @param string $name
+	 * @return mixed|null
+	 */
+	final public function get(string $name)
+	{
+		return $this->arguments[$name] ?? null;
+	}
+
+	/**
+	 * Args constructor.
+	 * @param array $defaultArguments
+	 *
+	 * @return Arguments $this;
+	 */
+	final public function __construct(array $defaultArguments = [])
+	{
+		$this->arguments = $defaultArguments;
+
+		return $this;
+	}
+}
